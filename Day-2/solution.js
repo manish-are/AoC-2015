f86efefc174b21038a5c465db242e6471fec545b
manish-are <manisharebsnl@gmail.com>
2023-06-20 14:19:03 +0530

    part-2 solved

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -27,4 +27,24 @@ fs.readFile(filePath,'utf8',(err,data) => {
 		//console.log(curr[0],curr[1],curr[2],acc);
 		return acc;
 	},0));
+
+	//part-2
+	console.log(data.trim().split('\n').map(x => x.split('x')).map(x => x.map(x => Number(x))).reduce((acc,curr) => {
+		let min1 = 0;
+		let min2 = 0;
+		if (curr[0] <= curr[1] && curr[0] <= curr[2] ){
+			min1 = curr[0];
+			min2 = curr[1] <= curr[2] ? curr[1] : curr[2];
+		}else if(curr[1] <= curr[0] && curr[1] <= curr[2]) {
+			min1 = curr[1];
+			min2 = curr[0] <= curr[2] ? curr[0] : curr[2];
+		}else if (curr[2] <= curr[0] && curr[2] <= curr[1]){
+			min1 = curr[2];
+			min2 = curr[0] <= curr[1] ? curr[0] : curr[1];
+ 		}
+		acc += min1 + min1 + min2 + min2 + (curr[0] * curr[1] * curr[2]);
+		return acc;
+	},0));
+
+
 });
